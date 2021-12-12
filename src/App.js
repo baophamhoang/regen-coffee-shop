3951f7f0d5643c006361ada999a788336a19cd66
Baodeptry <noobfromdn@gmail.com>
2021-12-12 15:16:03 +0700

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand} from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
+      <Navbar dark color="primary">
+        <div className='container'>
+          <NavbarBrand href='/'>Logo Brand</NavbarBrand>
+        </div>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +25,12 @@ function App() {
         </a>
       </header>
     </div>
+    
+    
+    
   );
 }
+
+
 
 export default App;
